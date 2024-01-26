4bb0673b9b7ab011afecf277357c7eb334d1d763
Prince Vince <vincexermitano@gmail.com>
2024-01-25 18:50:52 -0800

    Add overview and goal sections to odyssey projects description page

--- src/components/Projects/Odyssey.jsx
+++ src/components/Projects/Odyssey.jsx
@@ -5,7 +5,7 @@ import { GoArrowUpRight } from "react-icons/go";
 const Odyssey = () => {
     return (
         <section id="odyssey">
-            <div className="hero">
+            <section className="hero">
                 <img
                     className="main"
                     src="/images/coursebannerimg.png"
@@ -33,7 +33,34 @@ const Odyssey = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
+            <section className="overview-goal">
+                <h1>OVERVIEW</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+            </section>
+            <img src="/images/odyssey_hero_screenshot.png" alt="Odyssey Hero Screenshot" />
+            <section className="overview-goal">
+                <h1>GOAL</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                </p>
+            </section>
         </section>
     );
 };
