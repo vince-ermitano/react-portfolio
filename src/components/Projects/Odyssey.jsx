54ce91c6f62ee525d303b3b961096e6c5b0ebbfe
Prince Vince <vincexermitano@gmail.com>
2024-02-02 19:59:39 -0800

    Add related text for 'Tasks' section of Odyssey desc page

--- src/components/Projects/Odyssey.jsx
+++ src/components/Projects/Odyssey.jsx
@@ -120,13 +120,13 @@ const Odyssey = () => {
                         </li>
                         <li>
                             Since the masterclass is an exclusive experience to
-                            those who pay for it, the website had to be <b>secure</b> {" "}
-                            and provide <b>limited access</b> to those who had the credentials
-                            to do so.
+                            those who pay for it, the website had to be{" "}
+                            <b>secure</b> and provide <b>limited access</b> to
+                            those who had the credentials to do so.
                         </li>
                         <li>
-                            It had to be <b>accessible</b> for users across all types
-                            of devices and browsers.
+                            It had to be <b>accessible</b> for users across all
+                            types of devices and browsers.
                         </li>
                     </ol>
                     <p>
@@ -146,32 +146,185 @@ const Odyssey = () => {
                         onMouseEnter={(e) => taskItemOnMouseEnter(e)}
                         onMouseLeave={(e) => taskItemOnMouseLeave(e)}
                     >
-                        <div className="task task1"></div>
-                        <div className="task details"></div>
+                        <div className="task task1">
+                            <div className="title">
+                                <h4>Visual Design</h4>
+                            </div>
+                        </div>
+                        <div className="task details">
+                            <div className="description">
+                                <p>
+                                    Planning the look of the website is crucial
+                                    for me, so that when I get to the HTML and
+                                    CSS, I have a clear goal of what I need to
+                                    build. Thus, my first task was building
+                                    wireframes for the site.{" "}
+                                </p>
+                                <br />
+                                <p>
+                                    All the wireframes were built using Figma,
+                                    where I would craft designs and layouts for
+                                    each page of the website. They were then
+                                    shared and discussed with Gaku to make sure
+                                    we were on the same page and that they fit
+                                    the style he intended for. We went for a
+                                    dynamic and interactive look to keep users
+                                    engaged.
+                                </p>
+                                <br />
+                                <p>
+                                    I don't really see myself as creative when
+                                    it comes to visual design, especially when
+                                    it comes to layout and animations, so I did
+                                    take a lot of inspiration from the likes of
+                                    Dribbble, Codepen, and more. Of course, the
+                                    wireframes didn't always come out perfect
+                                    the first time through, so there was
+                                    constant analysis and iteration to
+                                    throughout the project's development.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div
                         className="task-item"
                         onMouseEnter={(e) => taskItemOnMouseEnter(e)}
                         onMouseLeave={(e) => taskItemOnMouseLeave(e)}
                     >
-                        <div className="task task1"></div>
-                        <div className="task details"></div>
+                        <div className="task task1">
+                            <div className="title">
+                                <h4>Database</h4>
+                            </div>
+                        </div>
+                        <div className="task details">
+                            <div>
+                                <p>
+                                    A database was essential for this project. I
+                                    stored data such as user credentials, user
+                                    purchased items, the masterclass course
+                                    modules, newsletter list, and more in our
+                                    database. Having all this data stored here
+                                    allowed for one, true source, making it easy
+                                    for fetched data across different devices
+                                    and accounts to be consistent and not
+                                    out-of-date.
+                                </p>
+                                <br />
+                                <p>
+                                    I used Firebase Firestore as my choice of
+                                    database technology. It was my first time
+                                    using a storage system that doesn't use SQL
+                                    as it's language, but it provided a quick
+                                    and easy way to host my database and perform
+                                    CRUD operations to carry out the
+                                    functionalities of the website.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div
                         className="task-item"
                         onMouseEnter={(e) => taskItemOnMouseEnter(e)}
                         onMouseLeave={(e) => taskItemOnMouseLeave(e)}
                     >
-                        <div className="task task1"></div>
-                        <div className="task details"></div>
+                        <div className="task task1">
+                            <div className="title">
+                                <h4>Server</h4>
+                            </div>
+                        </div>
+                        <div className="task details">
+                            <div>
+                                <p>
+                                    The server consisted of several main routes
+                                    to handle requests made via the client.
+                                </p>
+                                <br />
+                                <ol>
+                                    <li>
+                                        Payments for items from the site
+                                        (masterclass, lightroom presets, and
+                                        other digital products). All data
+                                        related to a purchase (cart items, price
+                                        of each item, total price, discount
+                                        codes, etc.) were packaged up to be
+                                        passed into the Stripe or PayPal API
+                                        where the actual financial transaction
+                                        took place. I decided that using these
+                                        APIs was the most practical way to go
+                                        about handling payments as any thing
+                                        that has to deal with a user submitting
+                                        their financial info needs to be very
+                                        secure and can be very complex to build
+                                        from scratch.
+                                    </li>
+                                    <li>Fetching the course modules</li>
+                                    <li>
+                                        Fetching other digital products
+                                        (lightroom presets, LUTs, transitions)
+                                    </li>
+                                    <li>
+                                        Creating users and updating user info. I
+                                        used Firebase Auth and Firestore to
+                                        handle the storing of user credentials
+                                        and related info after any handling of
+                                        user info submissions.
+                                    </li>
+                                    <li>Signing up users to the newsletter</li>
+                                    <li>
+                                        Sending emails to the Odyssey's contact
+                                        email. In our 'Contact Us' section, we
+                                        have a form for users to message us
+                                        about any inquiries.
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
                     </div>
                     <div
                         className="task-item"
                         onMouseEnter={(e) => taskItemOnMouseEnter(e)}
                         onMouseLeave={(e) => taskItemOnMouseLeave(e)}
                     >
-                        <div className="task task1"></div>
-                        <div className="task details"></div>
+                        <div className="task task1">
+                            <div className="title">
+                                <h4>Authentication/Authorization</h4>
+                            </div>
+                        </div>
+                        <div className="task details">
+                            <div>
+                                <p>
+                                    There were multiple
+                                    authentication/authorization flows or
+                                    enforcements that were implemented.
+                                </p>
+                                <br />
+                                <ol>
+                                    <li>
+                                        Storing basic user credentials (username
+                                        and password) and checking against them
+                                        were handled by Firebase Auth.
+                                    </li>
+                                    <li>
+                                        Only users who had purchased the
+                                        masterclass were allowed to view the
+                                        associated modules.
+                                    </li>
+                                    <li>
+                                        Any user that had made an account but
+                                        had not yet purchased the masterclass
+                                        had one of the modules unlocked to
+                                        preview to aid them in the decision of
+                                        the purchasing the course.
+                                    </li>
+                                    <li>
+                                        Only users implemented accounts could access their purchases enforcement(downloads and course modules) from a user dashboard.
+                                    </li>
+                                    <li>We implemented a 'one location, one login' enforcement where users could only be logged in on one device at a time to limit account sharing.</li>
+                                    <li>Prior to the official launch, we had the website locked up for users without a password.</li>
+                                    <li>Users that purchased digital products without being logged in were sent an email containing the associated download links. These links were only accessible for 24 hours.</li>
+                                </ol>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
